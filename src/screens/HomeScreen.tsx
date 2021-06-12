bb8ab746db97f5b8c7912c5f2bf6383982e5f54d
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-12 18:43:09 -0300

    Show pokemon list from api

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
+import {ActivityIndicator} from 'react-native';
 import {Image} from 'react-native';
 import {Text} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
 import {styles} from '../theme/appTheme';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
+  const {simplePokemonList, isLoading, loadPokemons} = usePokemonPaginated();
+
   return (
     <>
       <Image
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-      <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => (
+          <Image
+            source={{uri: item.picture}}
+            style={{width: 100, height: 100}}
+          />
+        )}
+        //Pagination
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={
+          <ActivityIndicator
+            style={{
+              height: 100,
+            }}
+            size={20}
+            color="grey"
+          />
+        }
+      />
+      {/* <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
         Pokedex
-      </Text>
+      </Text> */}
     </>
   );
 };
